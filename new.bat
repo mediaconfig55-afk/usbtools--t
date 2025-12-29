a80c76fc8a980dc390657c29a372e6fe8bb2f704
mediaconfig55-afk <media.config.55@gmail.com>
2025-12-29 11:19:06 +0300

    Refactor new.bat for better organization and clarity
    Refactor batch script for improved structure and readability. Update versioning, menu options, and command handling.

--- new.bat
+++ new.bat
@@ -1,9 +1,4 @@
 @echo off
-echo >>> GITHUB TEST ISARETI 999 <<<
-pause
-setlocal EnableDelayedExpansion
-...
-
 setlocal EnableDelayedExpansion
 chcp 65001 >nul
 cd /d "%~dp0"
@@ -17,26 +12,19 @@ if %errorLevel% neq 0 (
     echo [HATA] Bu arac yonetici olarak calistirilmalidir.
     echo.
     pause
-    goto :EOF
+    exit /b
 )
 
 :: ======================================================
 :: SABIT AYARLAR
 :: ======================================================
-set "VERSION=7.4 TEST"
-
-echo.
-echo *** GITHUB GUNCELLEME TESTI AKTIF ***
-echo.
-pause
-
+set "VERSION=7.4"
 
 title Professional IT Support & USB Suite v%VERSION% - Code Emre Bilgin
-mode con: cols=110 lines=52
+mode con: cols=120 lines=55
 color 0B
 
-:: MENUYE GIRMEDEN ONCE TEMIZLIK (OTOMATIK TETIKLENME ENGELI)
-set choice=
+:: Ilk acilista otomatik calisma engeli
 echo.
 echo Devam etmek icin bir tusa basin...
 pause >nul
@@ -46,58 +34,72 @@ pause >nul
 :: ======================================================
 :MENU
 cls
-set choice=
+echo ==========================================================================================
+echo        USB TOOLS & SISTEM YONETIM KONSOLU   -   V%VERSION%
+echo                         CODE EMRE BILGIN
+echo ==========================================================================================
 echo.
-echo ##########################################################################################
-echo #                 USB YONETIM VE SISTEM BAKIM KONSOLU (V%VERSION%)                       #
-echo #                           ==== CODE EMRE BILGIN ====                                  #
-echo ##########################################################################################
+echo  --- USB / DISK ISLEMLERI ---
+echo  [1]  Diskleri Listele
+echo  [2]  Disk Detaylari
+echo  [3]  Disk Yazma Korumasi AKTIF
+echo  [4]  Disk Yazma Korumasi KALDIR
+echo  [5]  Akilli Format (NTFS / FAT32 / exFAT)
 echo.
-echo --- USB VE DISK ISLEMLERI ---             --- SISTEM BAKIM VE ONARIM ---
-echo [1]  Mevcut Diskleri Goruntule            [16] Sistem Dosyalarini Onar (SFC)
-echo [2]  Akilli Format (NTFS/FAT32/exFAT)     [17] Sabit Diski Tarama (CHKDSK)
-echo [3]  Yazma Korumasini ETKINLESTIR          [18] Disk Temizligi
-echo [4]  Yazma Korumasini KALDIR               [19] Grup Politikalari Guncelle
-echo [5]  Disk ve Bolum Detaylari               [20] Windows Guncelleme Durumu
+echo  --- SISTEM BILGILERI ---
+echo  [10] Bilgisayar Bilgileri
+echo  [11] IP Adresi
+echo  [12] CPU Bilgisi
+echo  [13] RAM Bilgisi
+echo  [14] Detayli Sistem Bilgisi
 echo.
-echo --- SISTEM BILGILERI ---                  --- AG VE NETWORK ---
-echo [6]  Bilgisayar Bilgileri                 [26] DNS Temizle
-echo [7]  IP Adresi                            [27] IP Yenile
-echo [8]  Windows Lisans Durumu                [28] Ping Test
-echo [9]  Detayli Sistem Bilgisi               [29] Tracert
-echo [10] CPU Bilgisi                          [30] Netstat
-echo [11] RAM Bilgisi
+echo  --- SISTEM BAKIM ---
+echo  [20] Sistem Dosya Onarimi (SFC)
+echo  [21] Disk Tarama (CHKDSK)
+echo  [22] Disk Temizligi
+echo  [23] Grup Politikalari Guncelle
+echo  [24] Windows Guncelleme Durumu
 echo.
-echo --- PRO TEKNISYEN ---
-echo [34] Wi-Fi Sifresini Goster
-echo [35] CIKIS
+echo  --- NETWORK ---
+echo  [30] DNS Temizle
+echo  [31] IP Yenile
+echo  [32] Ping Test
+echo  [33] Tracert
+echo  [34] Netstat
 echo.
-echo ##########################################################################################
-set /p choice="Islem seciniz: "
+echo  --- PROFESYONEL ---
+echo  [40] Wi-Fi Sifresi Goster
+echo.
+echo  [99] CIKIS
+echo.
+set /p secim="Islem seciniz: "
 
-if "%choice%"=="1" goto LIST
-if "%choice%"=="2" goto FORMAT_SELECTION
-if "%choice%"=="3" goto PROTECT
-if "%choice%"=="4" goto UNPROTECT
-if "%choice%"=="5" goto DETAIL
-if "%choice%"=="6" goto COMPUTER_INFO
-if "%choice%"=="7" goto IP
-if "%choice%"=="8" goto LICENSE
-if "%choice%"=="9" goto SYSINFO
-if "%choice%"=="10" goto CPUINFO
-if "%choice%"=="11" goto MEMORY
-if "%choice%"=="16" goto SFC
-if "%choice%"=="17" goto CHKDSK
-if "%choice%"=="18" goto CLEANUP
-if "%choice%"=="19" goto GPUPDATE
-if "%choice%"=="20" goto WINDOWSUPDATE
-if "%choice%"=="26" goto CLEAR_DNS
-if "%choice%"=="27" goto IP_RENEW
-if "%choice%"=="28" goto PING_TEST
-if "%choice%"=="29" goto TRACERT_TEST
-if "%choice%"=="30" goto NETSTAT_TEST
-if "%choice%"=="34" goto WIFI_PASSWORD
-if "%choice%"=="35" goto EXIT_APP
+if "%secim%"=="1"  goto DISK_LIST
+if "%secim%"=="2"  goto DISK_DETAIL
+if "%secim%"=="3"  goto DISK_PROTECT
+if "%secim%"=="4"  goto DISK_UNPROTECT
+if "%secim%"=="5"  goto DISK_FORMAT
+
+if "%secim%"=="10" goto PC_INFO
+if "%secim%"=="11" goto IP_INFO
+if "%secim%"=="12" goto CPU_INFO
+if "%secim%"=="13" goto RAM_INFO
+if "%secim%"=="14" goto SYS_INFO
+
+if "%secim%"=="20" goto SFC_SCAN
+if "%secim%"=="21" goto CHKDSK_SCAN
+if "%secim%"=="22" goto CLEANMGR_RUN
+if "%secim%"=="23" goto GPUPDATE_RUN
+if "%secim%"=="24" goto WIN_UPDATE
+
+if "%secim%"=="30" goto DNS_CLEAR
+if "%secim%"=="31" goto IP_RENEW
+if "%secim%"=="32" goto PING_TEST
+if "%secim%"=="33" goto TRACERT_TEST
+if "%secim%"=="34" goto NETSTAT_TEST
+
+if "%secim%"=="40" goto WIFI_PASS
+if "%secim%"=="99" goto EXIT_APP
 
 goto MENU
 
@@ -105,132 +107,129 @@ goto MENU
 :: FONKSIYONLAR
 :: ======================================================
 
-:LIST
+:DISK_LIST
 cls
-echo list disk > ds.txt
-diskpart /s ds.txt
-del ds.txt
+echo list disk > %temp%\ds.txt
+diskpart /s %temp%\ds.txt
+del %temp%\ds.txt
 pause
 goto MENU
 
-:FORMAT_SELECTION
+:DISK_DETAIL
 cls
-echo list disk > ds.txt
-diskpart /s ds.txt
-del ds.txt
-set /p diskID="Formatlanacak Disk No: "
-echo [1] NTFS  [2] FAT32  [3] exFAT
-set /p fs_choice="Secim: "
-if "%fs_choice%"=="1" set fs=ntfs
-if "%fs_choice%"=="2" set fs=fat32
-if "%fs_choice%"=="3" set fs=exfat
-set /p confirm="Disk silinecek. Onay (E/H): "
-if /I not "%confirm%"=="E" goto MENU
+set /p dno="Disk No: "
 (
-echo select disk %diskID%
+echo select disk %dno%
+echo detail disk
+)>%temp%\ds.txt
+diskpart /s %temp%\ds.txt
+del %temp%\ds.txt
+pause
+goto MENU
+
+:DISK_PROTECT
+cls
+set /p dno="Disk No: "
+(echo select disk %dno% & echo attributes disk set readonly)>%temp%\ds.txt
+diskpart /s %temp%\ds.txt
+del %temp%\ds.txt
+pause
+goto MENU
+
+:DISK_UNPROTECT
+cls
+set /p dno="Disk No: "
+(echo select disk %dno% & echo attributes disk clear readonly)>%temp%\ds.txt
+diskpart /s %temp%\ds.txt
+del %temp%\ds.txt
+pause
+goto MENU
+
+:DISK_FORMAT
+cls
+echo list disk > %temp%\ds.txt
+diskpart /s %temp%\ds.txt
+del %temp%\ds.txt
+set /p dno="Disk No: "
+echo [1] NTFS  [2] FAT32  [3] exFAT
+set /p fs="Secim: "
+if "%fs%"=="1" set fss=ntfs
+if "%fs%"=="2" set fss=fat32
+if "%fs%"=="3" set fss=exfat
+set /p onay="Tum veriler silinecek (E/H): "
+if /I not "%onay%"=="E" goto MENU
+(
+echo select disk %dno%
 echo clean
 echo create partition primary
-echo format fs=%fs% quick
+echo format fs=%fss% quick
 echo assign
-)>ds.txt
-diskpart /s ds.txt
-del ds.txt
+)>%temp%\ds.txt
+diskpart /s %temp%\ds.txt
+del %temp%\ds.txt
 pause
 goto MENU
 
-:PROTECT
-cls
-set /p diskID="Disk No: "
-(echo select disk %diskID% & echo attributes disk set readonly)>ds.txt
-diskpart /s ds.txt
-del ds.txt
-pause
-goto MENU
-
-:UNPROTECT
-cls
-set /p diskID="Disk No: "
-(echo select disk %diskID% & echo attributes disk clear readonly)>ds.txt
-diskpart /s ds.txt
-del ds.txt
-pause
-goto MENU
-
-:DETAIL
-cls
-set /p diskID="Disk No: "
-(echo select disk %diskID% & echo detail disk)>ds.txt
-diskpart /s ds.txt
-del ds.txt
-pause
-goto MENU
-
-:COMPUTER_INFO
+:PC_INFO
 cls
 hostname
 wmic computersystem get manufacturer,model
 pause
 goto MENU
 
-:IP
+:IP_INFO
 cls
 ipconfig | findstr /i "IPv4"
 pause
 goto MENU
 
-:LICENSE
-cls
-slmgr /xpr
-pause
-goto MENU
-
-:SYSINFO
-cls
-systeminfo
-pause
-goto MENU
-
-:CPUINFO
+:CPU_INFO
 cls
 wmic cpu get name
 pause
 goto MENU
 
-:MEMORY
+:RAM_INFO
 cls
 wmic memorychip get capacity,speed
 pause
 goto MENU
 
-:SFC
+:SYS_INFO
+cls
+systeminfo
+pause
+goto MENU
+
+:SFC_SCAN
 cls
 sfc /scannow
 pause
 goto MENU
 
-:CHKDSK
+:CHKDSK_SCAN
 cls
-set /p drv="Surucu Harfi: "
+set /p drv="Surucu harfi: "
 chkdsk %drv%: /f /r
 pause
 goto MENU
 
-:CLEANUP
+:CLEANMGR_RUN
 cleanmgr
 pause
 goto MENU
 
-:GPUPDATE
+:GPUPDATE_RUN
 gpupdate /force
 pause
 goto MENU
 
-:WINDOWSUPDATE
+:WIN_UPDATE
 wmic qfe list brief /format:table
 pause
 goto MENU
 
-:CLEAR_DNS
+:DNS_CLEAR
 ipconfig /flushdns
 pause
 goto MENU
@@ -260,7 +259,7 @@ netstat -an
 pause
 goto MENU
 
-:WIFI_PASSWORD
+:WIFI_PASS
 cls
 set /p wifi="Ag Adi: "
 netsh wlan show profile name="%wifi%" key=clear | findstr /i "Key Content"
